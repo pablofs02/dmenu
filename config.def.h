28e94f2922a28d3ecc9b27bad5f8802c055e9b0e
pablofs02 <pablofs02@tutanota.com>
2023-02-02 15:56:35 +0100

    Ocupa el máximo espacio

--- config.def.h
+++ config.def.h
@@ -14,7 +14,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 32;
+static unsigned int lines      = 42;
 
 /*
  * Characters not considered part of a word while deleting words
